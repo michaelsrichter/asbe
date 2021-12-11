c8cd287f18b4a99ca10a820f054845bdb5a4fe53
Mike Richter <mrichter@microsoft.com>
2021-12-11 13:58:26 -0500

    Add project files.

---
+++ asbe.send/ASBEConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asbe.send
+{
+    public class ASBEConfig
+    {
+        public string SBConnectionString { get; set; }
+        public string QueueName { get; set; }
+        public int ParallelDegrees { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
+    }
+}
---
+++ asbe.send/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Messaging.ServiceBus;
+using asbe.send;
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+var config = host.Services.GetRequiredService<IConfiguration>();
+
+var asbeConfig = config.GetRequiredSection("ASBEConfig").Get<ASBEConfig>();
+
+string hostName = config["HOSTNAME"];
+var client = new ServiceBusClient(asbeConfig.SBConnectionString);
+var sender = client.CreateSender(asbeConfig.QueueName);
+
+var parallelOptions = new ParallelOptions() { MaxDegreeOfParallelism = asbeConfig.ParallelDegrees };
+
+foreach (var item in config.AsEnumerable())
+{
+    Console.WriteLine($"{item.Key}: {item.Value}");
+}
+
+Parallel.For(asbeConfig.Start, asbeConfig.End, parallelOptions, async i =>
+{
+    var message = new ServiceBusMessage("hi")
+    {
+        Subject = "sent from host " + hostName
+    };
+    message.ApplicationProperties.Add("Type", "Hello");
+    message.ApplicationProperties.Add("Iteration", i);
+    message.ApplicationProperties.Add("ManagedThreadId", Thread.CurrentThread.ManagedThreadId);
+
+    Console.WriteLine($"Message Type: Hello");
+    Console.WriteLine($"Message Iteration: {i}");
+    Console.WriteLine($"Message ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+
+    await sender.SendMessageAsync(message);
+});
+
+
+await host.RunAsync();
