4d5ec3d02890ef6561ed740628869fda50c6da2c
Mike Richter <michaelsrichter@hotmail.com>
2021-12-12 16:33:50 -0500

    Delete docker-run.sh

--- docker-run.sh
+++
@@ -1,8 +0,0 @@
-docker run -d -t -i --name send$RANDOM \
--e ASBEConfig__SBConnectionString="Endpoint=sb://sb-asbe-31784.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hY7FmbYWTuWHHjCFZBej+DXXV+SgD6IfIzz9GaYONW0=" \
--e ASBEConfig__QueueName="queue1" \
--e ASBEConfig__ParallelDegrees=-1 \
--e ASBEConfig__Start=0 \
--e ASBECOnfig__RunConstant=true \
-ghcr.io/michaelsrichter/asbe:master
-
